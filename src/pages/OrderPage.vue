302bfa580a181a021438405b3bfa90b650eaa528
Max Avvakumov <zerotomastery97@gmail.com>
2023-06-16 18:05:58 +0600

    add orderpage

---
+++ src/pages/OrderPage.vue
@@ -0,0 +1,147 @@
+<template>
+  <main class="content container">
+    <div class="content__top">
+      <ul class="breadcrumbs">
+        <li class="breadcrumbs__item">
+          <a class="breadcrumbs__link" href="index.html">
+            Каталог
+          </a>
+        </li>
+        <li class="breadcrumbs__item">
+          <a class="breadcrumbs__link" href="cart.html">
+            Корзина
+          </a>
+        </li>
+        <li class="breadcrumbs__item">
+          <a class="breadcrumbs__link">
+            Оформление заказа
+          </a>
+        </li>
+      </ul>
+
+      <h1 class="content__title">
+        Корзина
+      </h1>
+      <span class="content__info">
+        3 товара
+      </span>
+    </div>
+
+    <section class="cart">
+      <form class="cart__form form" action="#" method="POST">
+        <div class="cart__field">
+          <div class="cart__data">
+            <label class="form__label" for="form_fio">
+              <input id="form_fio" class="form__input" type="text" name="name" placeholder="Введите ваше полное имя">
+              <span class="form__value">ФИО</span>
+            </label>
+
+            <label class="form__label" for="form_delivery">
+              <input id="form_delivery" class="form__input" type="text" name="address" placeholder="Введите ваш адрес">
+              <span class="form__value">Адрес доставки</span>
+            </label>
+
+            <label class="form__label" for="form_phone">
+              <input id="form_phone" class="form__input" type="tel" name="phone" placeholder="Введите ваш телефон">
+              <span class="form__value">Телефон</span>
+              <span class="form__error">Неверный формат телефона</span>
+            </label>
+
+            <label class="form__label" for="form_fio">
+              <input id="form_fio" class="form__input" type="email" name="email" placeholder="Введи ваш Email">
+              <span class="form__value">Email</span>
+            </label>
+
+            <label class="form__label" for="form_comment">
+              <textarea id="form_comment" class="form__input form__input--area" name="comments"
+                placeholder="Ваши пожелания"></textarea>
+              <span class="form__value">Комментарий к заказу</span>
+            </label>
+          </div>
+
+          <div class="cart__options">
+            <h3 class="cart__title">Доставка</h3>
+            <ul class="cart__options options">
+              <li class="options__item">
+                <label for="cart_sam" class="options__label">
+                  <input id="cart_sam" class="options__radio sr-only" type="radio" name="delivery" value="0" checked="">
+                  <span class="options__value">
+                    Самовывоз <b>бесплатно</b>
+                  </span>
+                </label>
+              </li>
+              <li class="options__item">
+                <label for="cart_curier" class="options__label">
+                  <input id="cart_curier" class="options__radio sr-only" type="radio" name="delivery" value="500">
+                  <span class="options__value">
+                    Курьером <b>500 ₽</b>
+                  </span>
+                </label>
+              </li>
+            </ul>
+
+            <h3 class="cart__title">Оплата</h3>
+            <ul class="cart__options options">
+              <li class="options__item">
+                <label for="pay_card" class="options__label">
+                  <input id="pay_card" class="options__radio sr-only" type="radio" name="pay" value="card">
+                  <span class="options__value">
+                    Картой при получении
+                  </span>
+                </label>
+              </li>
+              <li class="options__item">
+                <label for="pay_cash" class="options__label">
+                  <input id="pay_cash" class="options__radio sr-only" type="radio" name="pay" value="cash">
+                  <span class="options__value">
+                    Наличными при получении
+                  </span>
+                </label>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="cart__block">
+          <ul class="cart__orders">
+            <li class="cart__order">
+              <h3>Смартфон Xiaomi Redmi Note 7 Pro 6/128GB</h3>
+              <b>18 990 ₽</b>
+              <span>Артикул: 150030</span>
+            </li>
+            <li class="cart__order">
+              <h3>Гироскутер Razor Hovertrax 2.0ii</h3>
+              <b>4 990 ₽</b>
+              <span>Артикул: 150030</span>
+            </li>
+            <li class="cart__order">
+              <h3>Электрический дрифт-карт Razor Lil’ Crazy</h3>
+              <b>8 990 ₽</b>
+              <span>Артикул: 150030</span>
+            </li>
+          </ul>
+
+          <div class="cart__total">
+            <p>Доставка: <b>500 ₽</b></p>
+            <p>Итого: <b>3</b> товара на сумму <b>37 970 ₽</b></p>
+          </div>
+
+          <button class="cart__button button button--primery" type="submit">
+            Оформить заказ
+          </button>
+        </div>
+        <div class="cart__error form__error-block">
+          <h4>Заявка не отправлена!</h4>
+          <p>
+            Похоже произошла ошибка. Попробуйте отправить снова или перезагрузите страницу.
+          </p>
+        </div>
+      </form>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+};
+</script>
